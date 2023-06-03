06467250502f9ac6bae021130eb966d3a4dbcc3d
Jonathan Bijl <sannoayo@gmail.com>
2023-06-03 18:18:18 -0400

    Created user schema

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 app.listen(PORT, () => console.log(`Server Connected to port ${PORT}`))
+
+const connectDB = require("./db");
+connectDB();
+
+process.on("unhandledRejection", err => {
+    console.log(`An error occurred: ${err.message}`)
+    server.close(() => process.exit(1))
+  })
