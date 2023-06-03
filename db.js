8e0c89eac03361b90967a54588a09f7bf206434d
Jonathan Bijl <sannoayo@gmail.com>
2023-06-03 17:52:13 -0400

    reset

--- db.js
+++ db.js
@@ -1,10 +1,1 @@
 const Mongoose = require("mongoose")
-const localDB = `mongodb://localhost:27017/role_auth`
-const connectDB = async () => {
-  await Mongoose.connect(localDB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  console.log("MongoDB Connected")
-}
-module.exports = connectDB
